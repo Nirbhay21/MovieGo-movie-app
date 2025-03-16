cbf0b293b93bd3c984af5a5fc0ff9feb71937bc7
Nirbhay <nirbhaymarde43@gmail.com>
2025-03-16 18:05:27 +0530

    Solve failed to load module script: Expected a JavaScript module script but the server responded with a MIME type of text/jsx. Strict MIME type checking is enforced for module scripts per HTML spec. try-1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwind from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "0.0.0.0",
@@ -10,13 +9,11 @@ export default defineConfig({
       strict: false,
     },
   },
-  optimizeDeps: {
-    include: ['react', 'react-dom']
-  },
   plugins: [
     react({
-      include: "**/*.jsx",
+      jsxRuntime: 'automatic',
+      include: '**/*.jsx',
     }),
-    tailwindcss()
-  ],
+    ...tailwind(),
+  ]
 })
